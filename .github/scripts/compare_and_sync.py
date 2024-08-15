4b7a0cf3f77f0762e4275ee93dc3642062abae3d
Robson Thiago <rtech.thiago@gmail.com>
2024-08-14 23:59:17 -0300

    Hotfix: Teste Sync Tamplate

--- .github/scripts/compare_and_sync.py
+++ .github/scripts/compare_and_sync.py
@@ -1,57 +1,42 @@
 import os
-import subprocess
-import shutil
+import filecmp
 from pathlib import Path
 import difflib
 
-def compare_directories(local_dir, remote_dir, exclude_file, exclude_file2):
+def compare_file_contents(local_dir, remote_dir, exclude_file):
     differences = []
     for root, _, files in os.walk(local_dir):
         for file in files:
             local_file = Path(root) / file
             remote_file = Path(remote_dir) / local_file.relative_to(local_dir)
-            
-            if local_file.name == exclude_file and exclude_file2:
+
+            if local_file.name == exclude_file:
                 continue
-            
-            if not local_file.exists():
-                differences.append(f"File {local_file} does not exist in the local repository.")
-                # differences.append(f"File {remote_file} does not exist in the remote repository.")
+
+            if not remote_file.exists():
+                differences.append(f"File {remote_file} does not exist in the remote repository.")
             else:
-                with open(local_file) as lf, open(remote_file) as rf:
+                with open(local_file, 'r') as lf, open(remote_file, 'r') as rf:
                     local_content = lf.readlines()
                     remote_content = rf.readlines()
                     diff = list(difflib.unified_diff(local_content, remote_content, fromfile=str(local_file), tofile=str(remote_file)))
                     if diff:
-                        differences.append(f"Differences found in {remote_file}:")
+                        differences.append(f"Differences found in {local_file}:")
                         differences.extend(diff)
+    
     return differences
-
-def sync_directories(local_dir, remote_dir, exclude_file, exclude_file2):
-    for root, _, files in os.walk(remote_dir):
-        for file in files:
-            if file == exclude_file and exclude_file2:
-                continue
-            remote_file = Path(root) / file
-            local_file = Path(local_dir) / remote_file.relative_to(remote_dir)
-            if not local_file.exists() or not filecmp.cmp(local_file, remote_file):
-                shutil.copy2(remote_file, local_file)
-                print(f"Synced {remote_file} to {local_file}")
 
 def main():
     local_dir = ".github"
     remote_dir = "remote_repo/.github"
-    exclude_file = "check-template"
-    exclude_file2 = "scripts/compare_and_sync.py"
+    exclude_file = "check-template.yml"
     
-    differences = compare_directories(local_dir, remote_dir, exclude_file, exclude_file2)
+    differences = compare_file_contents(local_dir, remote_dir, exclude_file)
     
     if differences:
         print("::error::Differences found between .github directories:")
         for diff in differences:
             print(diff)
-        print("::error::Syncing different files from remote repository to local.")
-        sync_directories(local_dir, remote_dir, exclude_file, exclude_file2)
         exit(1)
     else:
         print("No differences found between .github directories.")
