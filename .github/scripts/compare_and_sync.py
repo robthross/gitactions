15aa43e0faba8eeccc912cff920e67bc7d102cf0
Robson Thiago <rtech.thiago@gmail.com>
2024-08-14 23:30:08 -0300

    Hotfix: Teste Sync Tamplate

--- .github/scripts/compare_and_sync.py
+++ .github/scripts/compare_and_sync.py
@@ -4,14 +4,14 @@ import shutil
 from pathlib import Path
 import difflib
 
-def compare_directories(local_dir, remote_dir, exclude_file):
+def compare_directories(local_dir, remote_dir, exclude_file, exclude_file2):
     differences = []
     for root, _, files in os.walk(local_dir):
         for file in files:
             local_file = Path(root) / file
             remote_file = Path(remote_dir) / local_file.relative_to(local_dir)
             
-            if local_file.name == exclude_file:
+            if local_file.name == exclude_file or exclude_file2:
                 continue
             
             if not remote_file.exists():
@@ -40,7 +40,8 @@ def sync_directories(local_dir, remote_dir, exclude_file):
 def main():
     local_dir = ".github"
     remote_dir = "remote_repo/.github"
-    exclude_file = "check-template"
+    exclude_file = "check-template.yml"
+    exclude_file2 = "scripts/compare_and_sync.py"
     
     differences = compare_directories(local_dir, remote_dir, exclude_file)
     
@@ -49,7 +50,7 @@ def main():
         for diff in differences:
             print(diff)
         print("::error::Syncing different files from remote repository to local.")
-        sync_directories(local_dir, remote_dir, exclude_file)
+        sync_directories(local_dir, remote_dir, exclude_file, exclude_file2)
         exit(1)
     else:
         print("No differences found between .github directories.")
