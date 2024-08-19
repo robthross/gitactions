d7c9d9bb570f61fefcb548ebdf14f4eaeb2a86c1
Robson Thiago <rtech.thiago@gmail.com>
2024-08-19 18:54:36 -0300

    Hotfix: Teste Sync Tamplate

--- .github/scripts/compare_yaml.py
+++ .github/scripts/compare_yaml.py
@@ -50,7 +50,6 @@ def compare_and_sync_directories(local_dir, remote_dir, fixed_keys):
 
     if files_changed:
         print("Differences were found and synchronized. Pipeline will be aborted.")
-        # sys.exit(1)
     else:
         print("No differences found. Pipeline will continue.")
 
